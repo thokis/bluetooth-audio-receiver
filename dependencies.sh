9ab5db49a3e7ab570bb3d16016f89b7c51bb4f6e
Thomas Kiss <thokis@hotmail.de>
2019-04-26 15:30:20 +0200

    some finetuning

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S python2 python2-gobject python2-dbus pulseaudio pulseaudio-bluetooth alsa-utils pulsemixer bluez bluez-utils --needed
+pacman -S python2 python2-gobject python2-dbus pulseaudio pulseaudio-bluetooth alsa-utils pulsemixer bluez bluez-utils sudo --needed
